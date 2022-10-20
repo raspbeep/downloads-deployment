b7cd7844c751ba4773d3db169bc2d0b2ae373e86
Pavel Kratochvil <pakratoc@redhat.com>
2022-10-20 08:45:23 +0200

    Add error handling

--- main.go
+++ main.go
@@ -38,15 +38,10 @@ func architecturesPaths() [][]string {
 func writeIndex(path, message string) error {
 	file, err := os.Create(path)
 	if err != nil {
-		// TODO
+		log.Fatal(err)
 	}
 
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-
-		}
-	}(file)
+	defer closeFile(file)
 
 	fileContentSlice := []string{
 		"<!doctype html>",
@@ -63,210 +58,168 @@ func writeIndex(path, message string) error {
 
 	length, err := file.WriteString(fileContent)
 
-	if err != nil || len(fileContent) != length {
-		// TODO
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(fileContent) != length {
+		log.Fatal("Error writing to file")
 	}
 
 	return nil
 }
 
-func createTar(path, fileName, fileNameNoExt string) error {
-	target := fmt.Sprintf("%s.tar", filepath.Join(path, fileNameNoExt))
-	tarFile, err := os.Create(target)
+func createTar(path, fileName, fileNameNoExt string) {
+	tarFile, err := os.Create(fmt.Sprintf("%s.tar", filepath.Join(path, fileNameNoExt)))
 	if err != nil {
-		// TODO
+		log.Fatal(err)
 	}
-	defer func(tarFile *os.File) {
-		err := tarFile.Close()
-		if err != nil {
-			// TODO
-		}
-	}(tarFile)
+	defer closeFile(tarFile)
 
 	info, err := os.Stat(filepath.Join(path, fileName))
 	if err != nil {
-		// TODO
+		log.Fatal(err)
 	}
 
 	header, err := tar.FileInfoHeader(info, info.Name())
 	if err != nil {
-		//  TODO
+		log.Fatal(err)
 	}
 
 	tarWriter := tar.NewWriter(tarFile)
 	err = tarWriter.WriteHeader(header)
 	if err != nil {
-		// TODO
+		log.Fatal(err)
 	}
 	file, err := os.Open(filepath.Join(path, fileName))
 	if err != nil {
-		// TODO
+		log.Fatal(err)
 	}
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-			// TODO
-		}
-	}(file)
+	defer closeFile(file)
 
-	_, err = io.Copy(tarWriter, file)
-	if err != nil {
-		// TODO
+	if _, err = io.Copy(tarWriter, file); err != nil {
+		log.Fatal(err)
 	}
-
-	return nil
 }
 
-func createZip(path, fileName, fileNameNoExt string) error {
+func closeFile(file *os.File) {
+	err := file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createZip(path, fileName, fileNameNoExt string) {
 	target := fmt.Sprintf("%s.zip", filepath.Join(path, fileNameNoExt))
 	zipFile, err := os.Create(target)
 	if err != nil {
-		// TODO
+		log.Fatal(err)
 	}
-	defer func(zipFile *os.File) {
-		err := zipFile.Close()
-		if err != nil {
-			// TODO
-		}
-	}(zipFile)
+	defer closeFile(zipFile)
 
 	zipWriter := zip.NewWriter(zipFile)
 
 	file, err := os.Open(filepath.Join(path, fileName))
 	if err != nil {
-		// TODO
+		log.Fatal(err)
 	}
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-			// TODO
-		}
-	}(file)
+	defer closeFile(file)
 
 	writer, err := zipWriter.Create(fileName)
 	if err != nil {
-		// TODO
+		log.Fatal(err)
 	}
 
 	if _, err := io.Copy(writer, file); err != nil {
-		// TODO
+		log.Fatal(err)
 	}
-
-	return nil
 }
 
 func main() {
-
 	// should launch for each thread
 	tmpDir, err := os.MkdirTemp("", "tmpdir")
 	if err != nil {
-		// TODO
-		return
+		log.Fatal(err)
 	}
 
 	defer func(name string) {
 		err := os.Remove(name)
 		if err != nil {
-			// TODO
-			return
+			log.Fatal(err)
 		}
 	}(tmpDir)
 
-	fmt.Printf("serving from %s\n", tmpDir)
 	if err = os.Chdir(tmpDir); err != nil {
-		// TODO
-		return
+		log.Fatal(err)
 	}
 
 	for _, arch := range architectures() {
+		// TODO: correct permission?
 		if err = os.Mkdir(arch, 0777); err != nil && os.IsExist(err) {
-			// TODO
-			return
+			log.Fatal(err)
 		}
 	}
 
 	// create content
 	// change to /usr/share/openshift/LICENSE
 	if err = os.Symlink(basePath+"LICENSE", "oc-license"); err != nil {
-		// TODO
-		return
+		log.Fatal(err)
 	}
 
-	var content = []string{"<a href='oc-license'>license</a>"}
+	var content = "<ul>\n<li>\n<a href=\"oc-license\">license</a>\n</li>\n"
 
 	// assign values to 3 variables in for loop from architectures_paths()
 	for _, archPath := range architecturesPaths() {
 		arch, operatingSystem, pathToArch := archPath[0], archPath[1], archPath[2]
 		baseName := path.Base(pathToArch)
+		noExtName := strings.TrimSuffix(baseName, filepath.Ext(baseName))
+		archivePathRoot := path.Join(tmpDir, arch, operatingSystem)
 		targetPath := filepath.Join(arch, operatingSystem, baseName)
-		// targetPath := path.Join(arch, operatingSystem, baseName)
+
 		if err = os.Mkdir(path.Join(tmpDir, arch, operatingSystem), 0777); err != nil {
-			// TODO
-			return
+			log.Fatal(err)
 		}
 
 		if err = os.Symlink(pathToArch, path.Join(tmpDir, targetPath)); err != nil {
-			// TODO
-			return
+			log.Fatal(err)
 		}
 
-		noExtName := strings.TrimSuffix(baseName, filepath.Ext(baseName))
-		archivePathRoot := path.Join(tmpDir, arch, operatingSystem)
-		if err = createTar(archivePathRoot, baseName, noExtName); err != nil {
-			// TODO
-			return
-		}
+		createTar(archivePathRoot, baseName, noExtName)
+		createZip(archivePathRoot, baseName, noExtName)
 
-		if err = createZip(archivePathRoot, baseName, noExtName); err != nil {
-			// TODO
-			return
-		}
-		content = append(content, fmt.Sprintf("<a href=\"%s\">oc (%s %s)</a> (<a href=\"%s.tar\">tar</a> <a href=\"%s.zip\">zip</a>)",
+		content += fmt.Sprintf("<li><a href=\"%s\">oc (%s %s)</a> (<a href=\"%s.tar\">tar</a> <a href=\"%s.zip\">zip</a>)</li>\n",
 			targetPath,
 			arch,
 			operatingSystem,
 			filepath.Join(arch, operatingSystem, noExtName),
 			filepath.Join(arch, operatingSystem, noExtName),
-		))
+		)
 	}
-
-	tmpDirBase := filepath.Base(tmpDir)
-
-	//writeAllIndexes(tmpDir, tmpDir)
+	content += "</ul>\n"
 
 	if err = filepath.WalkDir(tmpDir+"/", func(path string, d os.DirEntry, err error) error {
-		// skip tmpDir
-		if d.Name() != tmpDirBase {
-			// put listings in each subdirectory
+		// create index files in subdirectories
+		if d.Name() != filepath.Base(tmpDir) {
+			// put indexes in each subdirectory
 			if d.IsDir() {
-
-				if err = writeIndex(
-					filepath.Join(path, "index.html"),
-					fmt.Sprintf("<p>Directory listings are disabled. See <a href=\"/\">here</a> for available content.</p>")); err != nil {
-					// TODO
+				subdirectoryIndexMsg := "<p>Directory listings are disabled. See <a href=\"/\">here</a> for available content.</p>"
+				if err = writeIndex(filepath.Join(path, "index.html"), fmt.Sprintf(subdirectoryIndexMsg)); err != nil {
+					log.Fatal(err)
 				}
 			}
 		} else {
-			formattedContent := []string{"<ul>"}
-			for _, c := range content {
-				formattedContent = append(formattedContent, fmt.Sprintf("  <li>%s</li>", c))
-			}
-			formattedContent = append(formattedContent, "</ul>")
-			indexFileContent := strings.Join(formattedContent[:], "\n")
-			if err = writeIndex(filepath.Join(path, "index.html"), indexFileContent); err != nil {
-				// TODO
+			// create main index file in root folder
+			if err = writeIndex(filepath.Join(path, "index.html"), content); err != nil {
+				log.Fatal(err)
 			}
 		}
-
 		return nil
 	}); err != nil {
-		// TODO
+		log.Fatal(err)
 	}
 
-	server := &http.Server{Addr: ":3333", Handler: http.FileServer(http.Dir(tmpDir))}
+	server := &http.Server{Addr: ":8080", Handler: http.FileServer(http.Dir(tmpDir))}
 	server.SetKeepAlivesEnabled(false)
-	fmt.Println("listening on port: 3333")
-
+	fmt.Println("listening on port: 8080")
 	log.Fatal(server.ListenAndServe())
 }
 
